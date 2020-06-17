498692502a9689bc7dd08f822849257bacf9a0a6
kwkt <44708388+kwQt@users.noreply.github.com>
2020-06-17 19:54:25 +0900

    Implement some parse functions

--- parser.cpp
+++ parser.cpp
@@ -19,19 +19,110 @@ std::shared_ptr<Token> Parser::getPeekToken() { return peekToken; }
 bool Parser::parse() { return parseTransitionUnit(); }
 
 bool Parser::parseTransitionUnit() {
-  while (true) {
+  while (curToken->getTokenType() != TOK_EOF) {
     auto func = parseFunctionDefinition();
     if (func) {
       TU->addFunction(std::move(func));
+    } else {
+      return false;
     }
-    if (curToken->getTokenType() == TOK_EOF) break;
-
     nextToken();
   }
   return true;
 }
 
-std::unique_ptr<ExprAST> Parser::parseExpression() {}
+std::unique_ptr<BinaryExprAST> Parser::parseBinaryOpExpr(
+    Precedence prec, std::unique_ptr<ExprAST> LHS) {
+  // TODO
+}
+
+std::unique_ptr<ExprAST> Parser::parseExpression() {
+  auto LHS = parsePrimary();
+  if (!LHS) return nullptr;
+
+  nextToken();
+
+  return parseBinaryOpExpr(LOWEST, std::move(LHS));
+}
+
+std::unique_ptr<ExprAST> Parser::parsePrimary() {
+  switch (curToken->getTokenType()) {
+    case TOK_IDENTIFIER:
+      return parseIdentifierExpr();
+
+    case TOK_NUMBER:
+      return parseNumberExpr();
+
+    case TOK_LPAREN:
+      return parseParenExpr();
+
+    default:
+      return nullptr;
+  }
+}
+
+std::unique_ptr<NumberAST> Parser::parseNumberExpr() {
+  double num = curToken->getTokenNum();
+  return std::make_unique<NumberAST>(num);
+}
+
+std::unique_ptr<ExprAST> Parser::parseIdentifierExpr() {
+  std::string name = curToken->getTokenString();
+
+  nextToken();
+
+  if (curToken->getTokenType() != TOK_LPAREN)
+    return std::make_unique<VariableAST>(name);
+
+  // Call
+  auto call = parseCallExpr(name);
+  if (!call) return nullptr;
+
+  return call;
+}
+
+std::unique_ptr<ExprAST> Parser::parseParenExpr() {
+  // eat (
+  nextToken();
+
+  auto expr = parseExpression();
+
+  if (curToken->getTokenType() != TOK_RPAREN) return nullptr;
+
+  // eat )
+  nextToken();
+
+  return expr;
+}
+
+std::unique_ptr<CallExprAST> Parser::parseCallExpr(
+    const std::string& callee_name) {
+  if (curToken->getTokenType() != TOK_LPAREN) return nullptr;
+
+  // eat (
+  nextToken();
+
+  std::vector<std::unique_ptr<ExprAST>> args;
+  auto arg = parseExpression();
+  while (arg) {
+    args.push_back(std::move(arg));
+
+    nextToken();
+
+    if (curToken->getTokenType() == TOK_RPAREN) break;
+
+    if (curToken->getTokenType() != TOK_COMMA) return nullptr;
+
+    nextToken();
+    arg = parseExpression();
+  }
+  if (!arg) return nullptr;
+
+  // eat )
+  nextToken();
+
+  return std::make_unique<CallExprAST>(callee_name, std::move(args));
+}
 
 std::unique_ptr<PrototypeAST> Parser::parsePrototype() {
   if (curToken->getTokenType() != TOK_IDENTIFIER) return nullptr;
@@ -39,9 +130,10 @@ std::unique_ptr<PrototypeAST> Parser::parsePrototype() {
 
   nextToken();
 
-  if (curToken->getTokenString() != "(") return nullptr;
+  if (curToken->getTokenType() != TOK_LPAREN) return nullptr;
 
-  nextToken();  // eat (
+  // eat (
+  nextToken();
 
   std::vector<std::string> args;
   while (curToken->getTokenType() == TOK_IDENTIFIER) {
@@ -49,9 +141,10 @@ std::unique_ptr<PrototypeAST> Parser::parsePrototype() {
     nextToken();
   }
 
-  if (curToken->getTokenString() != ")") return nullptr;
+  if (curToken->getTokenType() != TOK_RPAREN) return nullptr;
 
-  nextToken();  // eat )
+  // eat )
+  nextToken();
 
   return std::make_unique<PrototypeAST>(fn_name, std::move(args));
 }
--- parser.hpp
+++ parser.hpp
@@ -4,6 +4,23 @@
 #include "ast.hpp"
 #include "lexer.hpp"
 
+enum Precedence { LOWEST, SUM, PRODUCT, CALL };
+
+static Precedence getPrecedence(TokenType type) {
+  switch (type) {
+    case TOK_PLUS:
+    case TOK_MINUS:
+      return SUM;
+
+    case TOK_ASTERISK:
+    case TOK_SLASH:
+      return PRODUCT;
+
+    default:
+      return LOWEST;
+  }
+}
+
 class Parser {
  private:
   std::shared_ptr<Token> curToken;
@@ -24,4 +41,11 @@ class Parser {
   std::unique_ptr<PrototypeAST> parsePrototype();
   std::unique_ptr<FunctionAST> parseFunctionDefinition();
   std::unique_ptr<ExprAST> parseExpression();
+  std::unique_ptr<ExprAST> parsePrimary();
+  std::unique_ptr<NumberAST> parseNumberExpr();
+  std::unique_ptr<ExprAST> parseIdentifierExpr();
+  std::unique_ptr<ExprAST> parseParenExpr();
+  std::unique_ptr<CallExprAST> parseCallExpr(const std::string& callee_name);
+  std::unique_ptr<BinaryExprAST> parseBinaryOpExpr(
+      Precedence prec, std::unique_ptr<ExprAST> LHS);
 };
